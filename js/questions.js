bd700feacef958da2cf4db11de3ca44c6328b0a2
luis <bluerizabiz@gmail.com>
2022-05-01 20:48:29 -0500

    added the questions

---
+++ js/questions.js
@@ -0,0 +1,42 @@
+//Questions from W3schools
+
+
+
+var questions = [
+    {
+        title: "Inside which HTML element do we put the JavaScript",
+        choices: ["<script>", "<javascript>", "<scripting>", "js"],
+        answer: "<script>"
+    },
+    {
+        title: "Where is the correct place to insert a JavaScript?",
+        choices: ["Both the <head> section and the <body> section are correct", "The <body> section", "The <head> section"],
+        answer: "Both the <head> section and the <body> section are correct"
+    },
+    {
+        title: "The external JavaScript file must contain the <script> tag.",
+        choices: ["True", "False"],
+        answer: "False"
+    },
+    {
+        title: "How do you create a function in JavaScript?",
+        choices: ["function myFunction()", "function = myFunction()", "function:myFunction()"],
+        answer: "function myFunction()"
+    },
+    {
+        title: "How to write an IF statement in JavaScript?",
+        choices: ["if i = 5 then", "if (i == 5)", "if i = 5", "if i == 5 then"],
+        answer: "if (i == 5)"
+    },
+    {
+        title: "How can you add a comment in Javascript",
+        choices: ["//This is a comment", "<!--This is a comment-->", "'This is a comment"],
+        answer: "//This is a comment"
+    },
+    {
+        title: "How do you round the number 7.25, to the nearest integer?",
+        choices: ["Math.rnd(7.25)", "rnd(7.25)", "round(7.25)", "Math.round(7.25)"],
+        answer: "Math.round(7.25)"
+    }
+
+];
